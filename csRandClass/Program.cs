292fa2e0789aeabc2fc3fd9ec858fbd0e205c6d2
Turza <cse.turza@gmail.com>
2022-07-07 15:55:34 +0600

    pass stored in any sized array

--- csRandClass/Program.cs
+++ csRandClass/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Runtime.InteropServices;
 
 namespace csRandClass
 {
@@ -8,9 +7,10 @@ namespace csRandClass
         static void Main(string[] args)
         {
             var random = new Random(); //instance of random class
-            var buffer = new char[10]; //array to store the rand character
+            const int passwordLength = 10;
+            var buffer = new char[passwordLength]; //array to store the rand character
 
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < passwordLength; i++)
                 //Console.Write((char)random.Next(65, 122));
                 buffer[i] = (char)('a'+ random.Next(0, 26));
 
