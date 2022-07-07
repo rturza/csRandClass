0af3de43cbe8f95a69f9cae6ce402f561bc03caa
Turza <cse.turza@gmail.com>
2022-07-07 12:46:28 +0600

    Add project files.

---
+++ csRandClass/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csRandClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random(); //instance of random class
+            for (int i = 0; i < 10; i++)
+                Console.Write((char)random.Next(65, 122));
+            Console.WriteLine();
+        }
+    }
+}
